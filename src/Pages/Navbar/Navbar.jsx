25d61be45cf82652d8d4a42c5efda150ae3da3d9
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-12-14 15:33:33 +0600

    nev

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -18,70 +18,40 @@ const Navbar = () => {
 
 
                     <label htmlFor="my-modal-3" className="">
-                        <div className='flex items-center border-4 rounded text-[#9b1fe9] font-bold gap-2 px-3 py-1 border-[#9b1fe9]'>
+                        <div className='flex items-center border-4 rounded font-bold gap-2 px-3 py-1 '>
                             <span><FaWallet></FaWallet></span>
                             <span>Connect wallet</span>
                         </div>
                     </label>
 
-                    <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+
+                    <label htmlFor="my-wallet" className="">
+                        <div className='flex items-center border-4 rounded text-primary font-bold gap-2 px-3 py-1 border-primary'>
+                            <span><FaWallet></FaWallet></span>
+                            <span>Connect wallet</span>
+                        </div>
+                    </label>
+
+                    <input type="checkbox" id="my-wallet" className="modal-toggle" />
                     <div className="modal">
 
                         <div className="modal-box relative">
-                            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                            <label htmlFor="my-wallet" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                             <h3 className="text-2xl font-bold">Connect your wallet</h3>
 
 
                             <div className='my-5 grid justify-items-center lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 justify-items-center'>
                                 <div className='bg-gray-200 p-8'>
                                     <img className='mr-5' src="https://faucets.netlify.app/static/media/MetaMask.551edf10.svg" alt="" />
-                                    <h1 className='text-2xl text-[#9b1fe9] font-bold'>Metamask</h1>
+                                    <h1 className='text-2xl text-primary font-bold'>Metamask</h1>
 
                                 </div>
 
                                 <div className='bg-gray-200 p-8'>
                                     <img src="https://faucets.netlify.app/static/media/WalletConnect.d0b10794.svg" alt="" />
-                                    <h1 className='text-2xl text-[#9b1fe9] font-bold'>WalletConnect</h1>
+                                    <h1 className='text-2xl text-primary font-bold'>WalletConnect</h1>
                                 </div>
                             </div>
-
-
-
-
-
-                        </div>
-                    </div>
-                    <label htmlFor="my-modal-3" className="">
-                        <div className='flex items-center border-4 rounded text-[#9b1fe9] font-bold gap-2 px-3 py-1 border-[#9b1fe9]'>
-                            <span><FaWallet></FaWallet></span>
-                            <span>Connect wallet</span>
-                        </div>
-                    </label>
-
-                    <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-                    <div className="modal">
-
-                        <div className="modal-box relative">
-                            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                            <h3 className="text-2xl font-bold">Connect your wallet</h3>
-
-
-                            <div className='my-5 grid justify-items-center lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 justify-items-center'>
-                                <div className='bg-gray-200 p-8'>
-                                    <img className='mr-5' src="https://faucets.netlify.app/static/media/MetaMask.551edf10.svg" alt="" />
-                                    <h1 className='text-2xl text-[#9b1fe9] font-bold'>Metamask</h1>
-
-                                </div>
-
-                                <div className='bg-gray-200 p-8'>
-                                    <img src="https://faucets.netlify.app/static/media/WalletConnect.d0b10794.svg" alt="" />
-                                    <h1 className='text-2xl text-[#9b1fe9] font-bold'>WalletConnect</h1>
-                                </div>
-                            </div>
-
-
-
-
 
                         </div>
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   daisyui: {
-    themes: false,
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#9b1fe9",
+
+          "secondary": "#70ea8d",
+
+          "accent": "#44d9ff",
+
+          "neutral": "#25272D",
+
+          "base-100": "#ffffff",
+
+          "info": "#62B4EF",
+
+          "success": "#24CC6A",
+
+          "warning": "#EFB14E",
+
+          "error": "#FD6872",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 }
